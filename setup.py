6d803de4cd73ee5c2f1985a28739289247f21c6c
Doug Wiegley <doug@parksidesoftware.com>
2014-09-18 17:50:33 -0600

    pypi fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "a10-openstack-lbaas",
-    version = "1.0.0",
+    version = "1.0.1",
     packages = find_packages(),
 
     author = "A10 Networks",
